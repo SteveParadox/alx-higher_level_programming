67e664369a8309c6c8a078dbce7bcc64d528c4ec
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-03-13 01:56:39 +0100

    Create 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Function that removes all c and C from string"""
+
+
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
