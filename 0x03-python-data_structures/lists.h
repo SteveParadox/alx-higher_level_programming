6cd32abf9422a3f397254fe3d1e188f684be406d
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-03-13 02:44:21 +0100

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,35 +1,25 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-/* Define the structure of a single node in the linked list */
-typedef struct Node {
-    int data;
-    struct Node* next;
-} Node;
+/* Function prototypes */
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int is_palindrome(listint_t **head);
 
-/* Define the structure of the linked list */
-typedef struct LinkedList {
-    Node* head;
-    int size;
-} LinkedList;
-
-/* Function declarations */
-
-/* Creates a new linked list and returns a pointer to it */
-LinkedList* create_list();
-
-/* Adds a new node to the end of the linked list */
-void add_node(LinkedList* list, int data);
-
-/* Removes the first node with the specified data from the linked list */
-void remove_node(LinkedList* list, int data);
-
-/* Prints the contents of the linked list */
-void print_list(LinkedList* list);
-
-/* Frees the memory allocated to the linked list */
-void free_list(LinkedList* list);
-
-#endif /* LIST_H */
+#endif /* LISTS_H */
